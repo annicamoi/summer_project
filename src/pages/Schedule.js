d8c64532b2636954d2a412b8b5c3e1554d061216
hinnurainen <heidi.maattanen@gmail.com>
2021-05-28 15:39:29 +0300

    calendar added to schedule page

--- src/pages/Schedule.js
+++ src/pages/Schedule.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import TrainingTimetable from "../components/TrainingTimetable";
 
 const Schedule = () => {
     return (
         <div>
-
+            <TrainingTimetable />
         </div>
     );
 };
