f71dc86676616a0d81f3afd2a46e47b73758f8e9
hinnurainen <heidi.maattanen@gmail.com>
2021-06-02 18:17:50 +0300

    searchbox styling

--- src/components/SearchInstructor.js
+++ src/components/SearchInstructor.js
@@ -5,7 +5,7 @@ const SearchInstructor = ({ search }) => {
     return (
         <div>
             <Form>
-                <Form.Group className="justify-content-center"><Form.Control type="text" name="name" onChange={search} placeholder="Etsi ohjaajaa..." /></Form.Group>
+                <Form.Group className="justify-content-center"><Form.Control className="searchbox" type="text" name="name" onChange={search} placeholder="Etsi ohjaajaa..." /></Form.Group>
             </Form>
         </div>
     );
