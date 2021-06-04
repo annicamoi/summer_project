8f9896d0886e1ec554f4f5720160d7ffbe9928cd
Moilanen Annika <s2100141@edu.bc.fi>
2021-06-04 10:52:25 +0300

    color fixing & small bugs

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -3,27 +3,23 @@ import React from "react";
 export const Contact = () => (
   <div>
     <h2>Yhteystiedot</h2>
-    <p>Osoite:
-    Espoon TKD-seura ry
-    PL 9
-    02101 Espoo
-
-    Seuran pääopettaja:
-    Teppo Häyrynen
-    puh: 050-5232683
-    email: teppo.hayrynen@gmail.com
-
-    Hallituksen puheenjohtaja:
-    Tiia Takalo
-    puh: 050-3282512
-    email: takalotiia@gmail.com
-
-    Rahastonhoitaja:
-    Katariina Timonen
-    email: maksut@teamespoo.com
-
-    Hallitus
-    Puheenjohtaja: Tiia Takalo
-Jäsenet: Santiago Gonzalez, Katariina Timonen, Nina Nieminen, Juha Kouvonen ja Mona Eväsoja</p>
+    <p>Osoite: Espoon TKD-seura ry PL 9 02101 Espoo</p>
+    <br></br>
+    <p>
+      Seuran pääopettaj: Teppo Häyrynen puh: 050-5232683 email:
+      teppo.hayrynen@gmail.com
+    </p>
+    <br></br>
+    <p>
+      Hallituksen puheenjohtaja: Tiia Takalo puh: 050-3282512 email:
+      takalotiia@gmail.com
+    </p>
+    <br></br>
+    <p>Rahastonhoitaja: Katariina Timonen email: maksut@teamespoo.com</p>
+    <br></br>
+    <p>
+      Hallitus Puheenjohtaja: Tiia Takalo Jäsenet: Santiago Gonzalez, Katariina
+      Timonen, Nina Nieminen, Juha Kouvonen ja Mona Eväsoja
+    </p>
   </div>
 );
