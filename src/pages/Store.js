e9315073284bbfb42f547690a5b38229bb90473f
Moilanen Annika <s2100141@edu.bc.fi>
2021-06-10 14:01:45 +0300

    sidebar for news

--- src/pages/Store.js
+++ src/pages/Store.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import React from "react";
 
 const Store = () => {
-    return (
-        <div>
-            <h2>Osta pois, saat kaksi kolmen hinnalla!</h2>
-        </div>
-    );
+  return (
+    <div>
+      <h2>Osta pois, saat kaksi kolmen hinnalla!</h2>
+      <p>Shopify nettikauppa tässä</p>
+    </div>
+  );
 };
 
 export default Store;
